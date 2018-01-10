4ad91514a527c2b8f374b60e4376a31b3bb90ee0
Divik Mittal <33899376+divik544@users.noreply.github.com>
2018-01-10 14:33:12 +0530

    added bisection_method.cpp

---
+++ bisection_method.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+float func(float x)		//change this function as per requirement
+{
+	return (x*log10(x)-1.2f);
+}
+
+int main()
+{
+	float a,b;
+	float abserr = 0.001f;		//absolute error
+	int steps=0;
+	printf("Enter interval points:\n");
+	scanf("%f %f",&a,&b);	//enter a s.t. func(a)<0 && func(b)>0
+	if(func(a)*func(b)>0)
+		printf("invalid interval -_-\n");
+	else
+	{
+		float prevx= (a+b)/(float)2;
+		float curx;
+		int i=0;
+		bool res=false;
+		while(i++<100)	//no of iterations must not exceed 100
+		{
+			steps++;
+			if(func(prevx)<0)
+			a=prevx;
+			else
+			b=prevx;
+			
+			curx= (a+b)/(float)2;
+			if(fabs(prevx-curx)<abserr)
+			{
+				printf("answer found in %d iterations and ans is %.4f\n",steps,curx);
+				cout << func(prevx)<<" " <<func(curx);
+				res=true;
+				break;
+			}
+			prevx=curx;
+		}
+		if(!res)
+		cout<<"Unable to find answer in 100 iterations\n";
+	}
+}

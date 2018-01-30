ee7a3b9740c94b80abadde88d03887a5045820a1
Divik Mittal <33899376+divik544@users.noreply.github.com>
2018-01-30 21:48:09 +0530

    improved formatting

--- bisection_method.cpp
+++ bisection_method.cpp
@@ -12,36 +12,36 @@ int main()
 {
 	float a,b;
 	float abserr = 0.001f;		//absolute error
-	int steps=0;
+	int steps = 0;
 	printf("Enter interval points:\n");
 	scanf("%f %f",&a,&b);	//enter a s.t. func(a)<0 && func(b)>0
 	if(func(a)*func(b)>0)
 		printf("invalid interval -_-\n");
 	else
 	{
-		float prevx= (a+b)/(float)2;
+		float prevx = (a+b)/(float)2;
 		float curx;
-		int i=0;
-		bool res=false;
+		int i = 0;
+		bool res = false;
 		while(i++<100)	//no of iterations must not exceed 100
 		{
 			steps++;
 			if(func(prevx)<0)
-			a=prevx;
+			a = prevx;
 			else
-			b=prevx;
+			b = prevx;
 			
-			curx= (a+b)/(float)2;
-			if(fabs(prevx-curx)<abserr)
+			curx = (a+b)/(float)2;
+			if( fabs(prevx-curx) < abserr ) 
 			{
-				printf("answer found in %d iterations and ans is %.4f\n",steps,curx);
-				cout << func(prevx)<<" " <<func(curx);
-				res=true;
+				printf("answer found in %d iterations and ans is %.4f\n", steps, curx);
+				printf("%f %f", func(prevx), func(curx));
+				res = true;
 				break;
 			}
-			prevx=curx;
+			prevx = curx;
 		}
 		if(!res)
-		cout<<"Unable to find answer in 100 iterations\n";
+		printf("Unable to find answer in 100 iterations\n");
 	}
 }
